ddb26fbf654456bf386cab65a2ab170a7883405c
Lydia Fritz <Lydia.Fritz@gcu.edu>
2020-04-03 16:06:01 -0700

    started Graph class

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,21 @@
 //Lydia's graph code
 
 public class Driver {
+	public static void main(String[] args) {
+		
+	}
+}
 
+class Graph{
+	private String[] counties = {"Mohave", "Coconino", "Navajo",
+			"Apache", "Greenlee", "Cochise", "Santa Cruz", "Pima",
+			"Pinal", "Graham", "Gila", "Yavapai", "La Paz", "Yuma",
+			"Maricopa"};
+	
+	private int [][] matrix = new int[15][15];
+	
+	public Graph(){
+		//load graph
+		
+	}
 }
